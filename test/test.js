736f297d67f151ae33d42abbbfa26c611cb3a9f3
Matthew McCall <matt@mattmccall.com>
2015-01-07 11:48:38 -0800

    Refactor indentation.

--- test/test.js
+++ test/test.js
@@ -89,9 +89,110 @@ describe('Parse Comments >', function () {
     	expect(result.lineComment).to.equal('');
         expect(result.lineExpression).to.exist;
         expect(result.lineExpression).to.equal('OH HEY ');
-
     	done();
+    });
 
+});
+
+describe('Parse Indentation >', function () {
+
+    it('Basic Test', function (done) {
+        var testLine = ' .  . HELLO WORLD';
+        var result = parser.extractIndentation(testLine, {});
+        expect(result.lineIndentation).to.exist;
+        expect(result.lineIndentation).to.equal(2);
+        expect(result.lineExpression).to.exist;
+        expect(result.lineExpression).to.equal(' HELLO WORLD');
+        done();
+    });
+
+    it('No Line Start Indicator', function (done) {
+        var testLine = '.  . HELLO WORLD';
+        var result = parser.extractIndentation(testLine, {});
+        expect(result.lineIndentation).to.exist;
+        expect(result.lineIndentation).to.equal(2);
+        expect(result.lineExpression).to.exist;
+        expect(result.lineExpression).to.equal(' HELLO WORLD');
+        done();
+    });
+
+    it('No Interspacing', function (done) {
+        var testLine = ' .. HELLO WORLD';
+        var result = parser.extractIndentation(testLine, {});
+        expect(result.lineIndentation).to.exist;
+        expect(result.lineIndentation).to.equal(2);
+        expect(result.lineExpression).to.exist;
+        expect(result.lineExpression).to.equal(' HELLO WORLD');
+        done();
+    });
+
+    it('Multi Interspacing', function (done) {
+        var testLine = ' .   . HELLO WORLD';
+        var result = parser.extractIndentation(testLine, {});
+        expect(result.lineIndentation).to.exist;
+        expect(result.lineIndentation).to.equal(2);
+        expect(result.lineExpression).to.exist;
+        expect(result.lineExpression).to.equal(' HELLO WORLD');
+        done();
+    });
+
+    it('Multi Tab Interspacing', function (done) {
+        var testLine = ' .      . HELLO WORLD';
+        var result = parser.extractIndentation(testLine, {});
+        expect(result.lineIndentation).to.exist;
+        expect(result.lineIndentation).to.equal(2);
+        expect(result.lineExpression).to.exist;
+        expect(result.lineExpression).to.equal(' HELLO WORLD');
+        done();
+    });
+
+    it('No Trailing Spacing', function (done) {
+        var testLine = ' . .HELLO WORLD';
+        var result = parser.extractIndentation(testLine, {});
+        expect(result.lineIndentation).to.exist;
+        expect(result.lineIndentation).to.equal(2);
+        expect(result.lineExpression).to.exist;
+        expect(result.lineExpression).to.equal('HELLO WORLD');
+        done();
+    });
+
+    it('Trailing Tab Spacing', function (done) {
+        var testLine = ' . .    HELLO WORLD';
+        var result = parser.extractIndentation(testLine, {});
+        expect(result.lineIndentation).to.exist;
+        expect(result.lineIndentation).to.equal(2);
+        expect(result.lineExpression).to.exist;
+        expect(result.lineExpression).to.equal('    HELLO WORLD');
+        done();
+    });
+
+    it('Trailing Period', function (done) {
+        var testLine = ' .      . HELLO WORLD.';
+        var result = parser.extractIndentation(testLine, {});
+        expect(result.lineIndentation).to.exist;
+        expect(result.lineIndentation).to.equal(2);
+        expect(result.lineExpression).to.exist;
+        expect(result.lineExpression).to.equal(' HELLO WORLD.');
+        done();
+    });
+
+    it('No indentation', function (done) {
+        var testLine = ' HELLO WORLD.';
+        var result = parser.extractIndentation(testLine, {});
+        expect(result.lineIndentation).to.not.exist;
+        expect(result.lineExpression).to.exist;
+        expect(result.lineExpression).to.equal(' HELLO WORLD.');
+        done();
+    });
+
+    it('Single indentation', function (done) {
+        var testLine = '.HELLO WORLD.';
+        var result = parser.extractIndentation(testLine, {});
+        expect(result.lineIndentation).to.exist;
+        expect(result.lineIndentation).to.equal(1);
+        expect(result.lineExpression).to.exist;
+        expect(result.lineExpression).to.equal('HELLO WORLD.');
+        done();
     });
 
 });

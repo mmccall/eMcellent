c217f6c1e7a120b0e8819b382be4285836a66f19
Matthew McCall <matt@mattmccall.com>
2015-02-02 10:44:22 -0800

    Clean up routine split.

--- test/test.js
+++ test/test.js
@@ -131,7 +131,7 @@ describe('Parse Indentation >', function () {
     });
 
     it('Multi Tab Interspacing', function (done) {
-        var testLine = ' .      . HELLO WORLD';
+        var testLine = ' .\t\t. HELLO WORLD';
         var result = parser.extractIndentation(testLine, {});
         expect(result.lineIndentation).to.exist;
         expect(result.lineIndentation).to.equal(2);
@@ -151,17 +151,17 @@ describe('Parse Indentation >', function () {
     });
 
     it('Trailing Tab Spacing', function (done) {
-        var testLine = ' . .    HELLO WORLD';
+        var testLine = ' . .\tHELLO WORLD';
         var result = parser.extractIndentation(testLine, {});
         expect(result.lineIndentation).to.exist;
         expect(result.lineIndentation).to.equal(2);
         expect(result.lineExpression).to.exist;
-        expect(result.lineExpression).to.equal('    HELLO WORLD');
+        expect(result.lineExpression).to.equal('\tHELLO WORLD');
         done();
     });
 
     it('Trailing Period', function (done) {
-        var testLine = ' .      . HELLO WORLD.';
+        var testLine = ' .\t. HELLO WORLD.';
         var result = parser.extractIndentation(testLine, {});
         expect(result.lineIndentation).to.exist;
         expect(result.lineIndentation).to.equal(2);
@@ -186,6 +186,99 @@ describe('Parse Indentation >', function () {
         expect(result.lineIndentation).to.equal(1);
         expect(result.lineExpression).to.exist;
         expect(result.lineExpression).to.equal('HELLO WORLD.');
+        done();
+    });
+
+});
+
+describe('Split Routines and Arguments >', function () {
+
+    it('Basic Test', function (done) {
+        var testLine = ' HELLO WORLD';
+        var result = parser.splitRoutinesAndArguments(testLine, {});
+        expect(result).to.exist;
+        expect(result.length).to.equal(2);
+        expect(result[0]).to.equal('HELLO');
+        expect(result[1]).to.equal('WORLD');
+        done();
+    });
+
+    it('No Indentation', function (done) {
+        var testLine = 'HELLO WORLD';
+        var result = parser.splitRoutinesAndArguments(testLine, {});
+        expect(result).to.exist;
+        expect(result.length).to.equal(2);
+        expect(result[0]).to.equal('HELLO');
+        expect(result[1]).to.equal('WORLD');
+        done();
+    });
+
+    it('Tab Indentation', function (done) {
+        var testLine = '\tHELLO WORLD';
+        var result = parser.splitRoutinesAndArguments(testLine, {});
+        expect(result).to.exist;
+        expect(result.length).to.equal(2);
+        expect(result[0]).to.equal('HELLO');
+        expect(result[1]).to.equal('WORLD');
+        done();
+    });
+
+    it('Empty Arguments', function (done) {
+        var testLine = 'HELLO  WORLD';
+        var result = parser.splitRoutinesAndArguments(testLine, {});
+        expect(result).to.exist;
+        expect(result.length).to.equal(3);
+        expect(result[0]).to.equal('HELLO');
+        expect(result[1]).to.equal('');
+        expect(result[2]).to.equal('WORLD');
+        done();
+    });
+
+    it('Present Arguments', function (done) {
+        var testLine = 'HELLO BIG WORLD';
+        var result = parser.splitRoutinesAndArguments(testLine, {});
+        expect(result).to.exist;
+        expect(result.length).to.equal(3);
+        expect(result[0]).to.equal('HELLO');
+        expect(result[1]).to.equal('BIG');
+        expect(result[2]).to.equal('WORLD');
+        done();
+    });
+
+    it('Last Line Arguments', function (done) {
+        var testLine = 'HELLO BIG OLD WORLD';
+        var result = parser.splitRoutinesAndArguments(testLine, {});
+        expect(result).to.exist;
+        expect(result.length).to.equal(4);
+        expect(result[0]).to.equal('HELLO');
+        expect(result[1]).to.equal('BIG');
+        expect(result[2]).to.equal('OLD');
+        expect(result[3]).to.equal('WORLD');
+        done();
+    });
+
+    it('Last Line Space', function (done) {
+        var testLine = 'HELLO BIG OLD WORLD ';
+        var result = parser.splitRoutinesAndArguments(testLine, {});
+        expect(result).to.exist;
+        expect(result.length).to.equal(4);
+        expect(result[0]).to.equal('HELLO');
+        expect(result[1]).to.equal('BIG');
+        expect(result[2]).to.equal('OLD');
+        expect(result[3]).to.equal('WORLD');
+        done();
+    });
+
+    it('Last Line Empty Arguments', function (done) {
+        var testLine = 'HELLO BIG OLD WORLD  ';
+        var result = parser.splitRoutinesAndArguments(testLine, {});
+        expect(result).to.exist;
+        expect(result.length).to.equal(5);
+        expect(result[0]).to.equal('HELLO');
+        expect(result[1]).to.equal('BIG');
+        expect(result[2]).to.equal('OLD');
+        expect(result[3]).to.equal('WORLD');
+        expect(result[4]).to.equal('');
         done();
     });
 

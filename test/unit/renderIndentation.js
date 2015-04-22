50fce66ec01715456bc068aaedda1fb99b556974
Matthew McCall <matt@mattmccall.com>
2015-04-22 10:58:39 -0700

    Basic Render Indentation Tests.

---
+++ test/unit/renderIndentation.js
@@ -0,0 +1,77 @@
+"use strict";
+
+var expect = require('chai').expect;
+var render = require('../../lib/render.js');
+
+var testObject = {};
+
+describe('Render Indentation >', function () {
+
+    it('Absent Test', function (done) {
+        var result = render.appendIndentation(testObject, "");
+        expect(result).to.exist;
+        expect(result).to.equal('');
+        done();
+    });
+
+    it('Zero Test', function (done) {
+        testObject.lineIndentation = 0;
+        var result = render.appendIndentation(testObject, "");
+        expect(result).to.exist;
+        expect(result).to.equal('');
+        done();
+    });
+
+    it('One Test Basic', function (done) {
+        testObject.lineIndentation = 1;
+        var result = render.appendIndentation(testObject, "");
+        expect(result).to.exist;
+        expect(result).to.equal(' .');
+        done();
+    });
+
+    it('Two Test Basic', function (done) {
+        testObject.lineIndentation = 2;
+        var result = render.appendIndentation(testObject, "");
+        expect(result).to.exist;
+        expect(result).to.equal(' ..');
+        done();
+    });
+
+    it('One Test Indentation', function (done) {
+        testObject.lineIndentation = 1;
+        testObject.lineIndentationSpace = true;
+        var result = render.appendIndentation(testObject, "");
+        expect(result).to.exist;
+        expect(result).to.equal(' . ');
+        done();
+    });
+
+    it('Two Test Indentation', function (done) {
+        testObject.lineIndentation = 2;
+        testObject.lineIndentationSpace = true;
+        var result = render.appendIndentation(testObject, "");
+        expect(result).to.exist;
+        expect(result).to.equal(' . . ');
+        done();
+    });
+
+    it('One Test Indentation False', function (done) {
+        testObject.lineIndentation = 1;
+        testObject.lineIndentationSpace = false;
+        var result = render.appendIndentation(testObject, "");
+        expect(result).to.exist;
+        expect(result).to.equal(' .');
+        done();
+    });
+
+    it('Two Test Indentation False', function (done) {
+        testObject.lineIndentation = 2;
+        testObject.lineIndentationSpace = false;
+        var result = render.appendIndentation(testObject, "");
+        expect(result).to.exist;
+        expect(result).to.equal(' ..');
+        done();
+    });
+
+});

0e3c0f7056b9b3cf17cb4908fa2bf49e0e95523c
Matthew McCall <matt@mattmccall.com>
2015-04-21 23:05:50 -0700

    Bulk Loader

--- test/load/README.md
+++ test/load/README.md
@@ -1,1 +1,5 @@
-Run all of the files.
+#Bulk File Tester
+
+Given the extension to and modification of the M/MUMPS language over time, bulk execution of files can ensure that 100% fidelity is maintained by this parser.  This loader allows you to specify a directory, which will be recursively scanned and each file evaluated.
+
+This is primarily used against the OSEHRA issued VISTA instance of .M files.
--- test/load/load.js
+++ test/load/load.js
@@ -2,85 +2,124 @@
 "use strict"
 
 var fs = require('fs');
-var _ = require('lodash');
 var expect = require('chai').expect;
 var eMcellent = require('../../index.js');
 var path = require('path');
 var recursive = require('recursive-readdir');
+var jsdiff = require('diff');
+var rimraf = require('rimraf');
 
-var config = {};
-
-config.src = "../../../VistA-M/Packages";
-config.target = "./target";
-
-
-config.throttle = 1;
+var fileArray = [];
+var config = require('./config.json');
 
 
-recursive(path.join(__dirname, config.src), ["*.zwr", ".DS_Store"], function(err, files) {
-
-	if (config.throttle) {
-		files.splice(config.throttle, files.length - config.throttle);
-	}
-
-	for (var i in files) {
-		console.log(files[i]);
-		processFile(files[i], function(err, data) {
-
-
-
-		});
-	}
-
-
-
-});
-
-
-function processFile(inputPath, callback) {
-
-	fs.readFile(inputPath, {encoding: 'utf8'}, function (err, data) {
-		if (err) {callback(err)};
-		var parseResults = eMcellent.parse(data);
-		console.log(fileResults);
-
-
-
-	});
-
-
-
+function output (entry, src, txt, json, diff, callback) {
+	var fileName = entry.split('/')[entry.split('/').length - 1];
+	fs.writeFileSync(path.join(__dirname, config.output) + '/' + fileName + '-src.txt', src, {encoding: 'utf8'});
+	fs.writeFileSync(path.join(__dirname, config.output) + '/' + fileName + '-dest.txt', txt, {encoding: 'utf8'});
+	fs.writeFileSync(path.join(__dirname, config.output) + '/' + fileName + '-json.txt', json, {encoding: 'utf8'});
+	fs.writeFileSync(path.join(__dirname, config.output) + '/' + fileName + '-diff.txt', diff, {encoding: 'utf8'});
+	callback();
 }
 
+function test (inputEntry) {
 
+	describe('Test File: ' + inputEntry, function () {
 
+		var tmpSRC;
+		var tmpJSON;
+		var tmpTXT;
 
-/*
-var walk = function (dir, done) {
-	var results = [];
-	fs.readdir(dir, function(err, list) {
-		if (err) return done(err);
-		var pending = list.length;
-		if (!pending) return done(null, results);
-		list.forEach(function(file) {
-			fs.stat(file, function(err, stat) {
-				if (stat && stat.isDirectory()) {
-					walk(file, function(err, res) {
-						results = results.concat(res);
-						if (!--pending) done(null, results);
-					});
+		it('Load Record', function(done) {
+			fs.readFile(inputEntry, {encoding: 'utf8'}, function(err, data) {
+				if (err) {
+					done(err);
 				} else {
-					results.push(file);
-					if (!--pending) done(null, results);
+					tmpSRC = data;
+					done();
 				}
 			});
 		});
-	});
 
+		it('Parse Record', function(done) {
+			tmpJSON = eMcellent.parse(tmpSRC);
+			done();
+		});
+
+		it('Render Record', function(done) {
+			tmpTXT = eMcellent.render(tmpJSON);
+			done();
+		});
+
+		it('Compare Record', function(done) {
+			jsdiff.diffLines(tmpSRC, tmpTXT, function(err, tmpDIFF) {
+            if (err) {
+                done(err);
+            } else {            	
+            	var diffErr = false;
+            	if (tmpDIFF.length !== 1) {
+            		diffErr = true;
+            	}
+                for (var i in tmpDIFF) {
+                	if (tmpDIFF[i].count || tmpDIFF[i].added || tmpDIFF[i].removed) {
+                		diffErr = true;
+                	}
+                }
+
+                if (diffErr) {
+                	if (config.output) {
+                		output(inputEntry, tmpSRC, tmpTXT, tmpJSON, tmpDIFF, function(err) {
+                			done(new Error('File diffs inconsistent, check output'));
+                		});	
+                	} else {
+                		done(new Error('File diffs inconsistent'));
+                	}
+                	
+                } else {
+                	done();
+                }
+            }
+        	});
+		});
+	});
 }
 
-walk(, function(err, results) {
+describe('Load Testing', function () {
 
-	console.log(results);
+	before(function(done) {
+		rimraf(path.join(__dirname, config.output), function(err) {
+			if (err) {
+				done(err);
+			} else {
+				fs.mkdir(path.join(__dirname, config.output), function(err) {
+					if (err) {
+						done(err);
+					} else {
+						done();
+					}
+				})
+			}
+		});
+	});
 
-});*/
+	before(function(done) {
+		recursive(path.join(__dirname, config.source), ["*.zwr", ".DS_Store"], function(err, files) {
+			if (config.throttle) {
+				if (config.throttle !== "0") {
+					files.splice(config.throttle, files.length - config.throttle);	
+				}
+			}			
+			fileArray = files;
+			done();
+		});
+	});
+
+	it('Initialize Test Load', function(done) {
+		for (var i in fileArray) {
+			test(fileArray[i]);
+		}
+		done();
+	});
+
+});
+	
